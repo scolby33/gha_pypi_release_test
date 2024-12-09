ce4690f137cb67a04198e7a958613ce92c34951f
Scott Colby <scott@scolby.com>
2024-12-09 13:56:03 +0100

    Initial commit of minimal code to test GHA.

---
+++ src/gha_pypi_release_test/__init__.py
@@ -0,0 +1,1 @@
+"""A test package for figuring out how GitHub Actions releases should work."""
---
+++ tests/test_import.py
@@ -0,0 +1,5 @@
+import gha_pypi_release_test
+
+
+def test_import():
+    assert gha_pypi_release_test
